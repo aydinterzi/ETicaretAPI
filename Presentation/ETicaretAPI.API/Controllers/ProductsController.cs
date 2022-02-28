4effca4554d3c60c42200aa9e32afb55b2fcca4c
Aydın Terzi <aydinterzi7@gmail.com>
2022-02-28 23:52:23 +0300

    onion architecture implemented

---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using ETicaretAPI.Application.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult GetProducts()
+        {
+            return Ok();
+        }
+    }
+}
